c46a5c266ec7082e8f5b7f54a99c38b236b36477
lmarsh3 <87446734+lmarsh3@users.noreply.github.com>
2022-05-09 15:45:23 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDP1lTKMMUoDslT7lKy4mXUeL4GkQMHCcByByPvS+vNE/1b4lA7dlGhbg4aUQkFrQDSEDorgzpahn+aJEX8LpZXM9HjMjYXwxI3IjrysDdy+tVeKSoE0X0Wtb10SHQV26MgxN+mcH3deCUe6C1la0U8BHX9Sr33WCx+nuYzOkXbXqsrscFTp6ysDVOULaWg591N5MBafUazgaABrDJSylWRPTZofevCHs3ozxZ/Jyg/c01Akz2115VKNxRtWpltdlUc9dURGSFTgWmY2SGeJ9B5Q+yl+t5t//ILJcudm0lQwE9HvqAxDGSXWwSi/irvDnOzftj4hEQ+EDYl6+fWF33D',
 	}  
 }
